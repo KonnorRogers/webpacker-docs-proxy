1f3bdd61a814b15f38424af3716abab22ec0cc51
ParamagicDev <konnor5456@gmail.com>
2020-10-13 16:13:51 -0400

    move turbolinks to the start

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -9,10 +9,11 @@ import NavButtonController from "./controllers/nav_button_controller.js";
 import HeaderLinkController from "./controllers/header_link_controller.js"
 import DocumentLinkController from "./controllers/document_link_controller.js"
 
+Turbolinks.start()
+
 const application = Application.start()
 application.register("nav-button", NavButtonController)
 application.register("header-link", HeaderLinkController)
 application.register("document-link", DocumentLinkController)
 
-Turbolinks.start()
 
