d56c77e381b55e98654a0e0ac2f5e54cef0d9133
Konnor Rogers <konnor5456@gmail.com>
2020-10-14 15:01:24 -0400

    Update header_link_controller.js

--- frontend/javascript/controllers/header_link_controller.js
+++ frontend/javascript/controllers/header_link_controller.js
@@ -2,12 +2,21 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
   connect() {
+      document.addEventListener("turbolinks:before-cache", this.wrapHeaders.bind(this))
     document.addEventListener("turbolinks:render", this.wrapHeaders.bind(this));
     document.addEventListener("turbolinks:load", this.wrapHeaders.bind(this));
     document.addEventListener("DOMContentLoaded", this.wrapHeaders.bind(this));
   }
 
   disconnect() {
+    document.removeEventListener(
+      "turbolinks:before-cache",
+      this.wrapHeaders.bind(this)
+    );
+    document.removeEventListener(
+      "turbolinks:load",
+      this.wrapHeaders.bind(this)
+    );
     document.removeEventListener(
       "turbolinks:render",
       this.wrapHeaders.bind(this)
