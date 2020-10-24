4965737a43e2bd7489f1be6c9a4eb4e7932a2bf4
ParamagicDev <konnor5456@gmail.com>
2020-10-23 20:59:38 -0400

    fix: issues with - routes

--- frontend/javascript/controllers/document_link_controller.js
+++ frontend/javascript/controllers/document_link_controller.js
@@ -19,7 +19,7 @@ export default class extends Controller {
         return;
       }
 
-      const mdFileRegex = /\.md(#[\w\s]*)?$/
+      const mdFileRegex = /\.md(#[\w\S]*)?$/
       if (
         anchor.href.startsWith(window.location.origin) &&
         mdFileRegex.test(anchor.href)
