7db05a935af3439a4383e02ce8ff24830ab7fe49
Konnor Rogers <konnor5456@gmail.com>
2020-11-10 00:04:24 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true
-  },
-  purge: {
-    layers: ["all"],
-    mode: "layers",
-    content: ["./output/**/*.html"],
+    purgeLayersByDefault: false
   },
   theme: {
     extend: {},
