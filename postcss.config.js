734e6ee3dfaf303236bb87424589768a614fdc5b
Konnor Rogers <konnor5456@gmail.com>
2020-11-10 00:03:39 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,24 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/**/*.html',
+    './src/**/*.liquid',
+    './src/**/*.md',
+    './frontend/javascript/**/*.js'
+  ],
+  output: './output/_bridgetown/static/css',
+  defaultExtractor: content => {
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+    return broadMatches.concat(innerMatches)
+  }
+})
+
 module.exports = {
   plugins: [
-    // require("postcss-import"),
     require('tailwindcss'),
     require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }
